95c5cc9c20f10abc8d0b0620499deaf0870d5dcf
UK-taniyama <uk.taniyama@gmail.com>
2022-05-29 01:22:31 +0900

    add Kotlin Extensions

--- quack-java/src/main/java/com/koushikdutta/quack/Extensions.kt
+++ quack-java/src/main/java/com/koushikdutta/quack/Extensions.kt
@@ -13,11 +13,57 @@ suspend fun QuackPromise.await(): Any {
                 if (it !is JavaScriptObject)
                     throw QuackException("JavaScript Error type not thrown")
                 val jo: JavaScriptObject = it
-                jo.quackContext.evaluateForJavaScriptObject("(function(t) { throw t; })").call(it);
-            }
-            catch (e: Throwable) {
+                jo.quackContext.evaluateForJavaScriptObject("(function(t) { throw t; })").call(it)
+            } catch (e: Throwable) {
                 resume.resumeWithException(e)
             }
         }
     }
+}
+
+fun QuackContext.putFromObject(coercion: QuackCoercion<Any, Any>): QuackContext {
+    QuackCoercions.putFromObject(this, coercion)
+    return this
+}
+
+fun QuackContext.putToObject(coercion: QuackCoercion<Any, Any>): QuackContext {
+    QuackCoercions.putToObject(this, coercion)
+    return this
+}
+
+fun QuackContext.putToDate(): QuackContext {
+    QuackCoercions.putToDate(this)
+    return this
+}
+
+fun QuackContext.putFromList(): QuackContext {
+    QuackCoercions.putFromList(this)
+    return this
+}
+
+fun QuackContext.putFromArray(): QuackContext {
+    QuackCoercions.putFromArray(this)
+    return this
+}
+
+fun QuackContext.putToList(): QuackContext {
+    QuackCoercions.putToList(this)
+    return this
+}
+
+fun QuackContext.putFromMap(): QuackContext {
+    QuackCoercions.putFromMap(this)
+    return this
+}
+
+fun QuackContext.putToMap(): QuackContext {
+    QuackCoercions.putToMap(this)
+    return this
+}
+
+fun QuackContext.putUtilCoercion(): QuackContext {
+    this.putFromMap().putToMap()
+    this.putFromArray()
+    this.putFromList().putToList()
+    return this
 }
--- quack-java/src/test/java/com/koushikdutta/quack/TestQuackKotlin.kt
+++ quack-java/src/test/java/com/koushikdutta/quack/TestQuackKotlin.kt
@@ -2,7 +2,6 @@ package com.koushikdutta.quack
 
 import org.junit.Assert
 import org.junit.Test
-import java.io.File
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.startCoroutine
@@ -46,4 +45,72 @@ class QuackKotlinTests {
 
         assert(ret == "hello")
     }
+
+    @Test
+    fun testJavaScriptToJava() {
+        QuackContext.create().use { quackContext ->
+            quackContext.putToMap().putToList()
+
+            val obj = quackContext.evaluate(
+                "({\"str\":\"S\",\"array\":[10,\"X\"],\"list\":[10,\"X\"],\"map\":{\"b\":\"B\"}})",
+                Map::class.java
+            )
+            Assert.assertEquals(4, obj.size.toLong())
+            val jsArray = obj["array"]
+            Assert.assertEquals(JavaScriptObject::class.java, jsArray!!.javaClass)
+            val array = quackContext.coerceJavaScriptToJava(
+                List::class.java,
+                jsArray
+            ) as List<*>
+            Assert.assertEquals(2, array.size.toLong())
+            Assert.assertEquals(10, array[0])
+            Assert.assertEquals("X", array[1])
+            val jsList = obj["list"]
+            Assert.assertEquals(JavaScriptObject::class.java, jsList!!.javaClass)
+            val list = quackContext.coerceJavaScriptToJava(
+                MutableList::class.java,
+                jsList
+            ) as List<*>
+            Assert.assertEquals(2, list.size.toLong())
+            Assert.assertEquals(10, list[0])
+            Assert.assertEquals("X", list[1])
+            val jsMap = obj["map"]
+            Assert.assertEquals(JavaScriptObject::class.java, jsMap!!.javaClass)
+            val map = quackContext.coerceJavaScriptToJava(
+                MutableMap::class.java,
+                jsMap
+            ) as Map<*, *>
+            Assert.assertEquals(1, map.size.toLong())
+            Assert.assertEquals("B", map["b"])
+            val str = obj["str"] as String?
+            Assert.assertEquals(str, "S")
+        }
+    }
+
+    @Test
+    fun testJavaScriptFromJava() {
+        QuackContext.create().use { quackContext ->
+            quackContext.putFromMap().putFromList().putFromArray()
+
+            val fn =
+                quackContext.evaluateForJavaScriptObject("(function(x){ return x===undefined ? 'undefined' : JSON.stringify(x)})")
+            val map: MutableMap<String, Any> =
+                HashMap()
+            map["b"] = "B"
+            val list: MutableList<Any> = ArrayList()
+            list.add(10)
+            list.add("X")
+            val arg: MutableMap<String, Any> =
+                HashMap()
+            arg["array"] = list.toTypedArray()
+            arg["list"] = list
+            arg["map"] = map
+            arg["str"] = "S"
+            Assert.assertEquals(
+                "{\"str\":\"S\",\"array\":[10,\"X\"],\"list\":[10,\"X\"],\"map\":{\"b\":\"B\"}}",
+                fn.call(arg)
+            )
+        }
+    }
+
 }
