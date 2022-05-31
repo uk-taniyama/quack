7b64652a3ab6e0a8194ea5c6e25dd9ecb16becce
UK-taniyama <uk.taniyama@gmail.com>
2022-05-31 12:07:17 +0900

    [publish]fix actions

---
+++ .github/workflows/download_artifact.sh
@@ -0,0 +1,28 @@
+set -x
+if [ ! "$GH_TOKEN"]
+then
+    exit -1
+fi
+
+OWNER=uk-taniyama
+REPOSITORY=quack
+
+curl -s \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token ${GH_TOKEN}" \
+    "https://api.github.com/repos/${OWNER}/${REPOSITORY}/actions/artifacts" \
+    > artifacts.json
+
+download() {
+    name=$1
+    path=$2
+    url=`cat artifacts.json | jq -r "[.artifacts[] | select(.name==\"$name\")][0].archive_download_url"`
+    curl -s -L -H "Authorization: token ${GH_TOKEN}" $url > work.zip
+    mkdir -p $path
+    unzip work.zip -d $path
+}
+
+download quickjs-windows quack-jni/src/main/resources/META-INF/
+download quickjs-linux quack-jni/src/main/resources/META-INF/
+download quickjs-macos quack-jni/src/main/resources/META-INF/
+download quickjs-android build/
